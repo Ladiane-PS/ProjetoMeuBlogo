91d7e697b7f79eac8f54a2f82e77210469aeaeaa
Ladiane P.S <aladydyljc@gmail.com>
2024-09-18 01:47:59 -0300

    atualização no template

--- meublog/blog/models.py
+++ meublog/blog/models.py
@@ -1,10 +1,10 @@
-# blog/models.py
+
 from django.db import models
 
+# blog/models.py
 class Post(models.Model):
     titulo = models.CharField(max_length=200)
     conteudo = models.TextField()
     data_publicacao = models.DateTimeField(auto_now_add=True)
+    imagem_destacada = models.ImageField(upload_to='images/', blank=True, null=True)
 
-    def _str_(self):
-        return self.titulo
--- meublog/meublog/urls.py
+++ meublog/meublog/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # Inclua as URLs do aplicativo 'blog'
 ]
+
+# Adiciona as configurações de arquivos de mídia
+if settings.DEBUG:  # Serve arquivos de mídia apenas no modo de desenvolvimento
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
